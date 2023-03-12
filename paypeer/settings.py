6f2614b1748851a79080c5c22af9b91a90d50ede
jibola <pro.ajibolaojo@gmail.com>
2023-03-12 16:16:17 +0100

    cors fixed

--- paypeer/settings.py
+++ paypeer/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -47,7 +48,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "paypeer.urls"
 
-CORS_ALLOWED_ORIGINS = list(filter(bool, os.environ.get("ALLOWED_ORIGINS", "").split(" ")))
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
